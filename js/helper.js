30d15b1721ad65e6b9b0c4d3f0020c7f47c24413
Vicky Vishal Sahu <vicky.vishal.sahu@gmail.com>
2017-02-28 19:32:38 +0530

    New DOM elements to display

--- js/helper.js
+++ js/helper.js
@@ -1,3 +1,8 @@
-var HTMLlist = '<ul class="list-entry"></ul>';
-var HTMLalbumStart = '<li><div class="album-entry"></div></li>';
-var HTMLname = '<em>%data%</em>';
+var HTMLmainList = '<ul class="mainlist-entry"></ul>';
+var HTMLartistStart = '<li><div class="name-entry"></div></li><br>';
+var HTMLartistName = '<p>%data%</p>';
+var HTMLartistLink = '<a href="#" id="link" onclick="searchAlbum(%data%)">Click to see albumbs</a>';
+var HTMLalbumStart = '<ol class="albumlist-entry" style="display: none"></ol>';
+var HTMLalbumName = '<li><div class="album-entry">%data%</div></li>';
+var HTMLsongStart = '<ol class="songlist-entry" style="display: none"></ol>';
+var HTMLsongName = '<li><div class="song-entry">%data%</div></li>';
