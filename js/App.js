543b85fe6eb61d334435339c87901966003437c5
Vicky Vishal Sahu <vicky.vishal.sahu@gmail.com>
2017-02-28 18:56:01 +0530

    New feature added
    feat: Now a new page will open to show artist 
    refactor: Code has been refactored, the ajax request is sent uniquely for each input.

--- js/App.js
+++ js/App.js
@@ -1,7 +1,7 @@
 window.onload=function(){
 
 var data = "%data%";
-var searchAlbums = function (query) {
+var searchArtist = function (query) {
     $.ajax({
         url: 'https://api.spotify.com/v1/search',
         data: {
@@ -9,21 +9,38 @@ var searchAlbums = function (query) {
             type: 'artist'
         },
         success: function (response) {
-			console.log(response);
-			var formattedlist = response.artists.items;
-			$('#results').append(HTMLlist);
-			formattedlist.forEach(function(music){
-				$('.list-entry:last').append(HTMLalbumStart);
-				var formattedName = HTMLname.replace(data, music.name);
-				$('.album-entry:last').append(formattedName);
-			})
+			var artistList = response.artists.items;
+			$('#results').append(HTMLmainList);
+			artistList.forEach(function(artist){
+				var artistName = HTMLartistName.replace(data, artist.name);
+				var artistId = artist.id;
+				var artistLink = HTMLartistLink.replace("#", "artistpage/artistpage.html").replace("link", artistId).replace(data, artistId);
+				$('.mainlist-entry:last').append(HTMLartistStart);
+				$('.name-entry:last').append(artistName, artistLink);
+			});
         }
-    });
+    }); 
+};
+
+var searchAlbum = function (id){
+	$.ajax({
+		type: "GET",
+		dataType: 'json',
+		cache: false,					
+		url:'https://api.spotify.com/v1/artists/' + id + '/albums',
+		success: function (response) { //this will return 20 object. Each object further has 20 albums for each artist
+			var albumList = response.items;
+			albumList.forEach(function(album){
+				var albumName = album.name;
+				console.log(albumName);
+			});
+		}
+	});
 };
 
 
 document.getElementById('search-form').addEventListener('submit', function (e) {
     e.preventDefault();
-    searchAlbums(document.getElementById('query').value);
+    searchArtist(document.getElementById('query').value);
 }, false);
 }
